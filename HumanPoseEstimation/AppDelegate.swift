c0adb0d92ee71e544e074bbf99946fbc509a75b5
Akhil Tolani <akhiltolani777@gmail.com>
2019-02-21 06:56:59 +0530

    change username

--- HumanPoseEstimation/AppDelegate.swift
+++ HumanPoseEstimation/AppDelegate.swift
@@ -11,5 +11,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillEnterForeground(_ application: UIApplication) {}
     func applicationDidBecomeActive(_ application: UIApplication) {}
     func applicationWillTerminate(_ application: UIApplication) {}
+	//standard stuff
 }
 
